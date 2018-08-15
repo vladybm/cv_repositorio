7d75574259e0a1c43b491935fe2b99304527805b
vladynator <ismael.benitez@salud-digna.org>
2018-08-15 10:29:37 -0600

    Se modificaron archivos principales y se agregaron iconos e imagenes

--- cv.js
+++ cv.js
@@ -1,5 +1,12 @@
 function myMenu() {
+
     var efecto = document.getElementById("menu");
+    var desplazar = document.getElementById("menu__article");
+    var ocultar = document.getElementById("menu__article");
+    var resetX = document.getElementById("menu__article ul");
+
     efecto.classList.toggle("ancho");
-    console.log("efecto");
+    desplazar.classList.toggle("desplazar");
+    resetX.classList.toggle("reset-x");
+    ocultar.classList.toggle("ocultar");
 }
